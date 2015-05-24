53b6b4bb8524e31d0df5e273160874155ad2c7ab
Quan Do <quandm@smartosc.com>
2015-05-24 20:22:31 +0700

    aaa

---
+++ googleApi/config.py
@@ -0,0 +1,13 @@
+# separator used by search.py, categories.py, ...
+SEPARATOR = ";"
+
+LANG            = "en_US" # can be en_US, fr_FR, ...
+ANDROID_ID      = "3988B3D9A477C544"
+GOOGLE_LOGIN    = "appforandroidcom@gmail.com"
+GOOGLE_PASSWORD = "tieungao1"
+AUTH_TOKEN      = None # "yyyyyyyyy"
+
+# force the user to edit this file
+if any([each == None for each in [ANDROID_ID, GOOGLE_LOGIN, GOOGLE_PASSWORD]]):
+    raise Exception("config.py not updated")
+
